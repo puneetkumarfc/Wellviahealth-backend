007b52dad6cc98b2688123009c2265fa56ca3cb4
puneetkumar <puneetrocks20@gmail.com>
2025-05-14 00:08:28 +0530

    Updated code to allow static folder files to be publicly accessible

--- src/main/java/com/wellvia/WellviaHealth/config/WebMvcConfig.java
+++ src/main/java/com/wellvia/WellviaHealth/config/WebMvcConfig.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -15,13 +13,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Get the absolute path to the uploads directory
-        Path uploadsDir = Paths.get("src/main/resources/static/uploads/specialization");
-        String uploadsAbsolutePath = uploadsDir.toFile().getAbsolutePath();
-
-        // Configure resource handler for specialization images
+        // Configure resource handler for specialization images using classpath
         registry.addResourceHandler("/resources/static/uploads/specialization/**")
-                .addResourceLocations("file:" + uploadsAbsolutePath + "/")
+                .addResourceLocations("classpath:/static/uploads/specialization/")
                 .setCachePeriod(3600) // Cache for 1 hour
                 .resourceChain(true);
     }
