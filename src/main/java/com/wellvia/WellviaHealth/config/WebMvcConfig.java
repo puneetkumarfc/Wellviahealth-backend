ec6b8b9efd621e098cf77138af853dd8c91ee50b
puneetkumar <puneetrocks20@gmail.com>
2025-05-14 00:27:28 +0530

    Updated code to allow static folder files to be publicly accessible

--- src/main/java/com/wellvia/WellviaHealth/config/WebMvcConfig.java
+++ src/main/java/com/wellvia/WellviaHealth/config/WebMvcConfig.java
@@ -13,9 +13,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Configure resource handler for specialization images using classpath
-        registry.addResourceHandler("/resources/static/uploads/specialization/**")
-                .addResourceLocations("classpath:/static/uploads/specialization/")
+        // Configure resource handler for external images
+        registry.addResourceHandler("/resources/external_img/**")
+                .addResourceLocations("file:/home/ec2-user/java code/resources/external_img/")
                 .setCachePeriod(3600) // Cache for 1 hour
                 .resourceChain(true);
     }
